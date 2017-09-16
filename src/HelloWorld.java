99c4cef5bd165d6b488aa4a38d912db4fa26ff87
kjanarth <kjanarth@gmail.com>
2017-09-17 00:14:24 +1000

    changed same line to test conflict

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("This is edited in the local");
-		System.out.println("Hello World");
+		System.out.println("This is edited in the localt");
+		System.out.println("Hello git.");
 		System.out.println("This is edited in remote");
 	}
 }
