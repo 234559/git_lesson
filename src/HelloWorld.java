004bacb37acc7865243a079cefd08edc3dbab0d6
234559 <kjanarth@gmail.com>
2017-09-17 00:11:46 +1000

    Added line in remote git hub repository

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.pringln("This is edited in the local");
+		System.out.println("This is edited in the local");
 		System.out.println("Hello World");
+		System.out.println("This is edited in remote");
 	}
 }
 
